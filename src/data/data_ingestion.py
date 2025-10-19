7f9e5438b43b78c02d0223f5c1b9ffdaabd72764
Yu An <buckslimyuan@gmail.com>
2025-10-19 17:45:42 -0400

    Fix playlist issue

--- src/data/data_ingestion.py
+++ src/data/data_ingestion.py
@@ -51,13 +51,7 @@ def download_youtube_wav(video_url: str, download_folder: Path) -> None:
 def extract_playlist_urls(playlist_url: str) -> List[str]:
     assert is_playlist_url(playlist_url), "URL must be a valid YouTube playlist"
 
-    with yt_dlp.YoutubeDL(
-        {
-            "quiet": True,
-            "extract_flat": True,
-            "playlistend": None,
-        }
-    ) as ydl:
+    with yt_dlp.YoutubeDL({"quiet": True, "extract_flat": True}) as ydl:
         info_dict = ydl.extract_info(playlist_url, download=False)
         video_urls = [entry["url"] for entry in info_dict["entries"]]
         return video_urls
